4f2d329cee9b40eaba284835ca651ea97d94f5f9
Shama Parveen <139287494+shamap01@users.noreply.github.com>
2024-12-27 06:57:37 +0530

    Create Code Implementation : src Navbar.js

---
+++ Code Implementation : src Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.scss';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="logo">LetsUpgrade</div>
+            <ul className="nav-links">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/courses">Courses</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
+      
